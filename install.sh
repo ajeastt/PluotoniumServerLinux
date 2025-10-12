b0b86e0a369c24f7871b1fd82fd2074887f1ab8f
Francesco Milano <28491164+framilano@users.noreply.github.com>
2025-10-12 11:29:04 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -139,7 +139,7 @@ if [ $winecheck == 'y' ] || [ $winecheck == '' ] || [ $winecheck == 'Y' ] ; then
     #Installing wine
     sudo mkdir -pm755 /etc/apt/keyrings
     sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
-    sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/jammy/winehq-jammy.sources
+    sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/noble/winehq-noble.sources
     
     sudo apt update -y
     sudo apt install --install-recommends winehq-staging -y
