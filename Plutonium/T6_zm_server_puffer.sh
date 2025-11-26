2f388922088363d235e8bf9626dd761e7af522f0
Adam East <east.adam@icloud.com>
2025-11-26 12:04:01 -0600

    maybe it works

--- Plutonium/T6_zm_server_puffer.sh
+++ Plutonium/T6_zm_server_puffer.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-# harden Wine for headless / panel use
 export WINEDEBUG=-all
 export WINEDLLOVERRIDES="mscoree,mshtml="
 export DISPLAY=:
 export XDG_RUNTIME_DIR=/tmp
 
 INSTALLDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-LOGFILE="$INSTALLDIR/../t6_puffer.log"
 
-# log everything when run from PufferPanel
-echo "==== $(date) starting T6_zm_server_puffer.sh ====" >> "$LOGFILE"
-exec >> "$LOGFILE" 2>&1
-
+# Pull from PufferPanel environment variables
 NAME="${SERVER_NAME:-T6 Zombies 1}"
 PAT="${GAME_PATH:-$INSTALLDIR/../T6Gamefiles}"
 KEY="${SERVER_KEY:-}"
@@ -20,6 +15,9 @@ CFG="${SERVER_CFG:-dedicated_zm.cfg}"
 PORT="${SERVER_PORT:-4977}"
 MODE="t6zm"
 MOD="${SERVER_MOD:-}"
+
+# Update Plutonium files
+"$INSTALLDIR/plutonium-updater" -d "$INSTALLDIR"
 
 echo -e "\033]2;Plutonium - $NAME - Server restart\007"
 echo "Visit plutonium.pw | Join the Discord (plutonium) for NEWS and Updates!"
